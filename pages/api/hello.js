56eaa5385c1b2eaf0657f4faff9a75624581fc61
roshray <roshanroy759@gmail.com>
2022-01-13 01:27:15 +0530

    Next auth

---
+++ pages/api/hello.js
@@ -0,0 +1,5 @@
+
+
+export default function helloAPI(req,res ) {
+  res.status(200).json({ name: 'John Doe' })
+}
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,10 +1,21 @@
-function Login() {
+import { getProviders, signIn } from "next-auth/react"
+
+function Login({ providers}) {
     return (
         <div>
-           <h1> This is login page</h1>        
+            <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
         </div>
     )
 }
 
 export default Login
-        
+    
+export async function gwtSeerverSideProps() {
+    const providers =  await getProviders()
+
+    return {
+        props: {
+            providers,
+        },
+    }
+}
