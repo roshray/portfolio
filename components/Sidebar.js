e0eebe881ec4df788a75a9cca371e8f76e4b4838
roshray <roshanroy759@gmail.com>
2022-01-16 22:58:16 +0530

    Component Center

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,11 +11,11 @@ import { signOut, useSession } from "next-auth/react"
 
 function Sidebar() {
     const { data: session, status } = useSession()
-    
+
     console.log(session)
 
     return (
-        <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+        <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
          <div className="space-y-4">
             <button  className="flex item-centre space-x-2 hover:text-white" onClick={() => signOut()}>
                 <HomeIcon className="h-5 w-5" />
@@ -48,7 +48,17 @@ function Sidebar() {
             </button>
             <hr className="border-t-[0.1px] border-gray-900"/>
             <p className="cursor-pointer hover:text-white">Playlist name ...</p>
-        
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+            <p className="cursor-pointer hover:text-white">Playlist name ...</p>
+                    
         
          </div>
         </div>
